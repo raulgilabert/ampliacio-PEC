942c7a97ea3848dbd9646cb344f52439d9d308ac
MrWiLL0 <will04will@gmail.com>
2024-06-19 11:06:50 +0200

    Update multi.vhd

--- multi.vhd
+++ multi.vhd
@@ -23,6 +23,7 @@ entity multi is
 			d_sys_l   : IN std_LOGIc;
          except   : IN  STD_LOGIC;
          exc_code : IN  STD_LOGIC_VECTOR(3 DOWNTO 0);
+			wrd_fpu_l : IN STD_LOGIC;
          ldpc      : OUT STD_LOGIC;
          wrd       : OUT STD_LOGIC;
          wr_m      : OUT STD_LOGIC;
@@ -36,6 +37,7 @@ entity multi is
          addr_d    : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
          addr_a    : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
          op        : OUT INST;
+			wrd_fpu	 : OUT STD_LOGIC;
 			d_sys		 : OUT STD_LOGIC;
             sys    : OUT STD_LOGIC;
             state     : OUT state_t
@@ -45,7 +47,7 @@ end entity;
 architecture Structure of multi is
 
     -- Aqui iria la declaracion de las los estados de la maquina de estados
-    SIGNAL state: state_t; 
+    SIGNAL state_s: state_t; 
     SIGNAL fp_op : boolean;
 
 begin
@@ -101,12 +103,13 @@ begin
             '0';
     wr_m <= wr_m_l when state_s = DEMW else '0';
     word_byte <= w_b when state_s = DEMW else '0';
-    ldpc <= ldpc_l when state_s = DEMW or state_s = SYSTEM else '0';
+    ldpc <= ldpc_l when (state_s = DEMW and not fp_op) or state_s = SYSTEM or state_s = FP3 else '0';
     in_d <= "10" when state_s = SYSTEM else in_d_l;
     addr_d <= "001" when state_s = SYSTEM else addr_d_l;
     addr_a <= "101" when state_s = SYSTEM else addr_a_l;
     op <= op_l;
     sys <= '1' when state_s = SYSTEM else '0';
+	 wrd_fpu <= wrd_fpu_l when state_s = DEMW else '0';
 
     state <= state_s;
 
